9601b3ce396efceb625cbbc9a982e75d248e9328
Joachim Lindqvist <lindqvist.joachim@gmail.com>
2017-01-28 19:00:46 +0100

    WIP: use TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,18 @@ var path = require("path");
 var webpack = require("webpack");
 
 module.exports = {
-  entry: "./index.js",
+  entry: "./index.tsx",
   output: { path: __dirname, filename: "bundle.js" },
+  resolve: {
+    extensions: ['', '.ts', '.tsx', '.js']
+  },
   module: {
     loaders: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react!ts-loader'
+      },
       {
         test: /\.js$/,
         loader: "babel-loader",
